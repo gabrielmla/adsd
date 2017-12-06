b1d53f9dc652361e5c8eb9b18984df154b12e8a4
gabrielmla <gabmla19@gmail.com>
2017-12-06 07:46:45 -0300

    melhorias...

--- scripts/run_1gb.sh
+++ scripts/run_1gb.sh
@@ -1,52 +1,7 @@
-echo "Starting..."
-sleep 1s
-
-echo "1GB exec"
-sleep 1s
-for i in {1..11}
-	do
-		echo "LOOP $I"		
-		
-		echo "Reseting counters..."
-		curl -X POST localhost:3000/depositApi/resetCounter
-		sleep 1s
-
-		echo "Post 50"
-		cgexec -g memory:myGroup ./req_post_50.sh
-		echo "Post 50 DONE"
-
-		echo "Post 150"
-		cgexec -g memory:myGroup ./req_post_150.sh
-		echo "Post 150 DONE"
-
-		echo "Put 50"
-		cgexec -g memory:myGroup ./req_put_50.sh
-		echo "Put 50 DONE"
-
-		echo "Put 150"
-		cgexec -g memory:myGroup ./req_put_150.sh
-		echo "Put 150 DONE"
-
-		echo "Deleting..."
-		source ./delete_dep.sh
-		echo "Delete DONE"
-
-		echo "Saving results..."
-		mkdir -p "../results/E$i/"
-		mv "../results/req_post_50.csv" "../results/E$i/"
-		mv "../results/db_post_50.csv" "../results/E$i/"
-		mv "../results/req_post_150.csv" "../results/E$i/"
-		mv "../results/db_post_150.csv" "../results/E$i/"
-		mv "../results/req_put_50.csv" "../results/E$i/"
-		mv "../results/db_put_50.csv" "../results/E$i/"
-		mv "../results/req_put_150.csv" "../results/E$i/"
-		mv "../results/db_put_150.csv" "../results/E$i/"
-		echo "Saving DONE"
-	done
-
 echo "500MB exec"
 sleep 1s
-for i in {11..21}
+
+for i in {1..11}
 	do
 		echo "LOOP $I"		
 		
@@ -90,7 +45,8 @@ echo "Done 500MB exec."
 
 echo "250MB exec"
 sleep 1s
-for i in {21..31}
+
+for i in {1..11}
 	do
 		echo "LOOP $I"		
 		
@@ -131,5 +87,3 @@ for i in {21..31}
 		echo "Saving DONE"
 	done
 echo "Done 250MB exec."
-
-echo "END"
